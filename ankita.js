360d6f29d0e5128c1346e5be411e12488207df62
Persis Shah <persisshah.123@gmail.com>
2020-05-25 18:03:41 +0530

    first commit

---
+++ ankita.js
@@ -0,0 +1,27 @@
+document.querySelector(".head1").addEventListener("mouseover" , function(){
+
+    document.querySelector(".head1").textContent="My Babe Cutie Pie !";
+
+});
+document.querySelector(".head1").addEventListener("mouseout" , function(){
+
+    document.querySelector(".head1").textContent="For Someone Special ";
+  });
+document.querySelector(".btn-2").addEventListener("click" , function(){
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+
+});
+document.querySelector(".myButton").addEventListener("click",function(){
+
+    var tom1 = new Audio ("Music/01-ek-ajnabee-haseena-se-128-kbps-downloadmingla_frTJNwcM.mp3") ;
+    tom1.play();
+
+
+});
+document.querySelector(".btn-3").addEventListener("click" , function(){
+document.querySelector(".dirty").textContent="Turn Me On with Your Best Weapon.";
+});
+document.querySelector(".btn-4").addEventListener("click" , function(){
+document.querySelector(".dirty-1").textContent="Send Me Nude Twerk photo & video";
+});
